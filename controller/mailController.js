7d3514d1c0b13ef132df515e5a55e6ffe8747e49
Akrem Hammami <akrem@gmx.us>
2021-05-10 11:46:42 +0200

    update

--- controller/mailController.js
+++ controller/mailController.js
@@ -1,5 +1,7 @@
-const nodemailer = require("nodemailer");
 
+var ElasticMail = require('nodelastic');
+var client = new ElasticMail('8DB27F009C592324AEAE54E9036D9F9665B977D242ED5441792655B329B07A48A67A74351BCBCA00D92D45883EF33005');
+const env= require('dotenv').config()
 
 exports.sendmail=async(req,res)=>{
 
@@ -7,38 +9,42 @@ exports.sendmail=async(req,res)=>{
     try{
     
 
-      
-     let  transport  = nodemailer.createTransport({
-
-        service:"gmail",
-        auth:{
-            user:'akrem.hammami.issatso@gmail.com',
-            pass:'Friendly05'
-        }
-    });
-        message ="email = "+req.body.email+req.body.nom+" tel = "+req.body.tel+" message = "+req.body.message;
-    let mailOptions={
-        from:req.body.email,
-        to:"akrem.hammami.issatso@gmail.com",
-        subject:req.body.nom,
-        text:message
-    };
-    
-    transport.sendMail(mailOptions,(err,data)=>{
-    
-        if(err){
-            console.log("error")
-        }else{
-            console.log("emailsend")
-        }
-    })
-    
-    
      
-    
-  
-    
-  
+       /*  var attachments = [ 
+            // CSV
+            { data: 'id,name\n1,name_1',
+              filename: 'attachment1.csv' },
+            // PDF
+            { data: fs.readFileSync('filepath_here'),
+              filename: 'attachment2.pdf',
+              contentType: 'application/pdf' } 
+          ]; */
+          
+          client.send({
+            from : 'akrem.hammami170498@gmail.com',
+            fromName : 'Sender',
+            subject : 'Subject',
+            msgTo: [ 'akrem@gmx.us' ],
+            bodyHtml: '<h1>Hello World</h1>',
+            textHtml: 'Hello World'
+          }).then(console.log);
+          
+          // will print 
+          // {"success":true,"data":{"transactionid":"190d1b03-8b01-41a1-8003-17181c1719b0","messageid":"ilXf1Nm38mxuxemecfdbvw2"}}
+          
+          // You can also set default options, for example having the same from and the name of the sender
+          // the function below is the same as the client.send above
+          client.setConfig({
+            from : 'akrem.hammami170498@gmail.com',
+            fromName : 'Sender'
+          });
+          client.send({
+            subject : 'Subject',
+            msgTo: [ 'akrem@gmx.us' ],
+            msgCC: [ 'cc_1@domain.com', 'cc_2@domain.com' ],
+            bodyHtml: '<h1>Hello World</h1>',
+            textHtml: 'Hello World'
+          }).then(console.log);
         res.status(200).json({
 
         
